2d06b3483809c23f2ef95c70056fdf61de0b91fc
smithwebtek <brad@smithwebtek.com>
2017-06-05 17:37:02 -0400

    update questions and links

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static#home'
   get '/import', to: 'static#new_import'
 	get '/get_map', to: 'static#get_map'
+
   resources :service_calls, :employee_service_calls, :employees, :truck_service_calls, :work_orders, :pools, :customers, :trucks, :inventories
 
  end
